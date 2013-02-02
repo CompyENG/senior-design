37406bdf29c96b42bf8db63f8526e3cf24ee5bf9
Bobby Graese <bobby.graese@gmail.com>
2013-02-02 02:52:36 -0600

    Using libusb version 1.0 instead
    Changes to some class functions, and a small function which should find
    the first PTP camera attahced to the computer. Untested :/

--- libs-cpp/libptp2.cpp
+++ libs-cpp/libptp2.cpp
@@ -1,6 +1,6 @@
 // A conversion of pyptp2 and all that comes with it
-//  Targeted at libusb version 0.1, since that's what pyptp2 uses
-#include "usb.h"
+//  Targeted at libusb version 1.0, since that's recommended
+#include "libusb.h"
 
 class CameraBase {
     
@@ -11,8 +11,8 @@ class CameraBase {
         bool _bulk_read(int size);
     public:
         CameraBase();
-        CameraBase(struct usb_device *dev);
-        bool open(struct usb_device *dev);
+        CameraBase(libusb_device *dev);
+        bool open(libusb_device *dev);
         bool close();
         bool reopen();
         bool send_ptp_message(char * bytestr, int timeout);
@@ -49,3 +49,57 @@ class CHDKCamera : public CameraBase {
         struct lv_data * get_live_view_data(bool liveview, bool overlay, bool palette);
         char * _wait_for_script_return(int timeout);
 };
+
+libusb_device * find_first_camera() {
+    // discover devices
+    libusb_device **list;
+    libusb_device *found = NULL;
+    ssize_t cnt = libusb_get_device_list(NULL, &list);
+    ssize_t i = 0, j = 0, k = 0;
+    int err = 0;
+    if (cnt < 0) {
+        return NULL;
+    }
+
+    for (i = 0; i < cnt; i++) {
+        libusb_device *device = list[i];
+        struct libusb_config_descriptor * desc;
+        int r = libusb_get_active_config_descriptor(device, &desc);
+        
+        if (r < 0) {
+            fprintf(stderr, "failed to get config descriptor");
+            return NULL;
+        }
+        
+        for(j = 0; j < desc->bNumInterfaces; j++) {
+            struct libusb_interface interface = desc->interface[j];
+            for(k = 0; k < interface.num_altsetting; k++) {
+                struct libusb_interface_descriptor altsetting = interface.altsetting[k];
+                if(altsetting.bInterfaceClass == 6) { // If this has the PTP interface
+                    found = device;
+                    break;
+                }
+            }
+            if(found) break;
+        }
+        
+        libusb_free_config_descriptor(desc);
+        
+        if(found) break;
+    }
+
+    /*
+    if (found) {
+        libusb_device_handle *handle;
+
+        err = libusb_open(found, &handle);
+        if (err)
+            error();
+        // etc
+    }
+
+    libusb_free_device_list(list, 1); // Can I do this here?
+    */
+    
+    return found;
+}
