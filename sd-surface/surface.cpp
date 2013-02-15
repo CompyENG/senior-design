f11ce98c513cd3ee9688a7203230bc5ef0cd2104
Bobby Graese <bobby.graese@gmail.com>
2013-02-15 12:56:06 -0600

    Add includes and shell to surface code

---
+++ sd-surface/surface.cpp
@@ -0,0 +1,13 @@
+#include <libptp++.hpp>
+#include <SDL/SDL.h>
+#include <iostream>
+#include <string>
+
+#include "surface.hpp"
+#include "SubJoystick.hpp"
+
+int main(int argv, char * argc[]) {
+    // TODO
+    
+    return 0;
+}
