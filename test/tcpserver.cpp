01773b4b28d3ce6d49bdbf0d8cd5b58353cf8f70
Shawn McCrimmon <shawn.mccrimmon@valpo.edu>
2013-02-24 15:21:41 -0600

    Test Networking Code

---
+++ test/tcpserver.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+#include <cstring>      // Needed for memset
+#include <sys/socket.h> // Needed for the socket functions
+#include <netdb.h>      // Needed for the socket functions
+
+int main()
+{
+
+    int status;
+    struct addrinfo host_info;       // The struct that getaddrinfo() fills up with data.
+    struct addrinfo *host_info_list; // Pointer to the to the linked list of host_info's.
+
+    // The MAN page of getaddrinfo() states "All  the other fields in the structure pointed
+    // to by hints must contain either 0 or a null pointer, as appropriate." When a struct
+    // is created in c++, it will be given a block of memory. This memory is not nessesary
+    // empty. Therefor we use the memset function to make sure all fields are NULL.
+    memset(&host_info, 0, sizeof host_info);
+
+    std::cout << "Setting up the structs..."  << std::endl;
+
+    host_info.ai_family = AF_UNSPEC;     // IP version not specified. Can be both.
+    host_info.ai_socktype = SOCK_STREAM; // Use SOCK_STREAM for TCP or SOCK_DGRAM for UDP.
+    host_info.ai_flags = AI_PASSIVE;     // IP Wildcard
+
+    // Now fill up the linked list of host_info structs with google's address information.
+    status = getaddrinfo(NULL, "5556", &host_info, &host_info_list);
+    // getaddrinfo returns 0 on succes, or some other value when an error occured.
+    // (translated into human readable text by the gai_gai_strerror function).
+    if (status != 0)  std::cout << "getaddrinfo error" << gai_strerror(status) ;
+
+
+    std::cout << "Creating a socket..."  << std::endl;
+    int socketfd ; // The socket descripter
+    socketfd = socket(host_info_list->ai_family, host_info_list->ai_socktype,
+                      host_info_list->ai_protocol);
+    if (socketfd == -1)  std::cout << "socket error " ;
+
+    std::cout << "Binding socket..."  << std::endl;
+    // we use to make the setsockopt() function to make sure the port is not in use
+    // by a previous execution of our code. (see man page for more information)
+    int yes = 1;
+    status = setsockopt(socketfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int));
+    status = bind(socketfd, host_info_list->ai_addr, host_info_list->ai_addrlen);
+    if (status == -1)  std::cout << "bind error" << std::endl ;
+
+    std::cout << "Listen()ing for connections..."  << std::endl;
+    status =  listen(socketfd, 5);
+    if (status == -1)  std::cout << "listen error" << std::endl ;
+
+
+    int new_sd;
+    struct sockaddr_storage their_addr;
+    socklen_t addr_size = sizeof(their_addr);
+    new_sd = accept(socketfd, (struct sockaddr *)&their_addr, &addr_size);
+    if (new_sd == -1)
+    {
+        std::cout << "listen error" << std::endl ;
+    }
+    else
+    {
+        std::cout << "Connection accepted. Using new socketfd : "  <<  new_sd << std::endl;
+    }
+
+
+    std::cout << "Waiting to recieve data..."  << std::endl;
+    ssize_t bytes_recieved;
+    char incomming_data_buffer[1000];
+    bytes_recieved = recv(new_sd, incomming_data_buffer,1000, 0);
+    // If no data arrives, the program will just wait here until some data arrives.
+    if (bytes_recieved == 0) std::cout << "host shut down." << std::endl ;
+    if (bytes_recieved == -1)std::cout << "recieve error!" << std::endl ;
+    std::cout << bytes_recieved << " bytes recieved :" << std::endl ;
+    incomming_data_buffer[bytes_recieved] = '\0';
+    std::cout << incomming_data_buffer << std::endl;
+
+
+    std::cout << "send()ing back a message..."  << std::endl;
+    char *msg = "thank you.";
+    int len;
+    ssize_t bytes_sent;
+    len = strlen(msg);
+    bytes_sent = send(new_sd, msg, len, 0);
+
+    std::cout << "Stopping server..." << std::endl;
+    freeaddrinfo(host_info_list);
+    close(new_sd);
+    close(socketfd);
+
+return 0 ;
+
+
+}
+
