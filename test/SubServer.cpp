6fa2438e7c7361e4fa2699561ae4baf430dfa46f
aseaofflames <aseaofflames@gmail.com>
2013-03-21 13:50:16 -0500

    fixed tabs and ip string

--- test/SubServer.cpp
+++ test/SubServer.cpp
@@ -18,7 +18,7 @@ SubClient::Subserver()
 
 bool SubServer::listen(int port)
 {
-	sock_desc = socket(AF_INET, SOCK_STREAM, 0); 
+    sock_desc = socket(AF_INET, SOCK_STREAM, 0); 
     if (sock_desc == -1)
     {
         printf("cannot create socket!\n");
@@ -59,41 +59,41 @@ bool SubServer::listen(int port)
 bool SubServer::send(uint32_t size_of_data, int8_t * data)
 {
 
-	int sent = 0;
-	sent = ::send(temp_sock_desc, &size_of_data, 4, 0);
-	while(sent < size_of_data) 
-	{
-		int bytes_sent = 0;
-		bytes_sent = ::send(temp_sock_desc, &data+sent, size_of_data-sent, 0);
-		if(bytes_sent == -1) {
-			std::cout << "Cannot write to server!" << std::endl;
-			return false;
-		}
-		sent += bytes_sent;
-	}
+    int sent = 0;
+    sent = ::send(temp_sock_desc, &size_of_data, 4, 0);
+    while(sent < size_of_data) 
+    {
+        int bytes_sent = 0;
+        bytes_sent = ::send(temp_sock_desc, &data+sent, size_of_data-sent, 0);
+        if(bytes_sent == -1) {
+            std::cout << "Cannot write to server!" << std::endl;
+            return false;
+        }
+        sent += bytes_sent;
+    }
     return true;
 }
 
 bool SubServer::recv(LVData * out) 
 {
-	int recvd = 0;
-	uint32_t size;
-	::recv(temp_sock_desc, &size, 4, 0);
-	while(recvd < size) 
-	{
-		int bytes_recvd;
-		bytes_recvd = ::recv(temp_sock_desc, &out+recvd, size-recvd, 0);
-		if(bytes_recvd == -1) {
-			std::cout << "Cannot receive data!" << std::endl;
-			return false;
-		}
-		recvd += bytes_recvd;
-	}
+    int recvd = 0;
+    uint32_t size;
+    ::recv(temp_sock_desc, &size, 4, 0);
+    while(recvd < size) 
+    {
+        int bytes_recvd;
+        bytes_recvd = ::recv(temp_sock_desc, &out+recvd, size-recvd, 0);
+        if(bytes_recvd == -1) {
+            std::cout << "Cannot receive data!" << std::endl;
+            return false;
+        }
+        recvd += bytes_recvd;
+    }
     return true;
 }
 
 void SubClient::disconnect()
 {
-	close(sock_desc);
-	close(temp_sock_desc);
+    close(sock_desc);
+    close(temp_sock_desc);
 }
