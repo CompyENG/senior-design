7c97b2f754e3f8f17e3aaf6e0139b9afc54e4591
Bobby Graese <bobby.graese@gmail.com>
2013-03-23 13:07:52 -0500

    Fix data hiding in client

--- sd-surface/SubClient.hpp
+++ sd-surface/SubClient.hpp
@@ -8,9 +8,11 @@
 
 class SubClient
 {
+    private:
+    struct sockaddr_in client;
+    int sock_desc;
+    
 	public:
-	struct sockaddr_in client;
-	int sock_desc;
 	bool connect(std::string ip, int port);
 	bool recv(LVData * data_out);
 	bool send(uint32_t size_of_data, int8_t * data);
